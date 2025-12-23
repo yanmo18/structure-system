d12256083924c35f64111b0c3fc4181b0c71dae8
衍青 <moyan548@foxmail.com>
2025-12-23 11:08:32 +0800

    添加导入包

--- sky/src/main.ts
+++ sky/src/main.ts
@@ -1,2 +1,24 @@
+// 从Vue组件库导入createApp和createPinia函数,用于创建Vue示例，状态管理
 import { createApp } from 'vue'
 
+import { createPinia } from 'pinia'
+
+//导入根组件App.vue,用于根容器
+import App from './App.vue'
+
+//导入路由实例，用于路由规则、路径
+import router from './router'
+
+//导入ant-design-vue样式组件库，并命名为Antd
+import Antd from "ant-design-vue";
+
+//导入样式 给所有浏览统一样式的
+import 'ant-design-vue/dist/reset.css';
+
+const app = createApp(App)//创建Vue应用实例，并传入根组件App
+//将导入的插件挂载到APP实例上面
+app.use(createPinia())//使用Pinia状态管理插件
+app.use(router)//使用路由插件
+app.use(Antd)
+
+app.mount('#app')//将APP实例挂载到id为app的元素上
--- sky/vite.config.ts
+++ sky/vite.config.ts
@@ -1,7 +1,30 @@
+import { fileURLToPath,URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //基本路径
+  base:'/bigscreen',
+
+server:{
+  //设置端口号
+  port:9091,
+  //开启自动打开浏览器
+  open:true,
+  //设置代理
+  proxy:{
+    '/smplatformApi':{
+      target:'http://10.20.10.87.7001',
+      changeOrigin:true
+
+    }
+  }
+},
+resolve:{
+  alias:{
+    '@':fileURLToPath(new URL('./src',import.meta.url))
+  }
+}
 })
